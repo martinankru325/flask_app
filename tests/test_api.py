8aebb995e3274fe0cea95132618eb3c4913fcb69
Martin <martinankru927@gmail.com>
2025-11-06 01:13:34 +0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,32 +1,35 @@
 import pytest
 from app.models import Client, Parking, ClientParking
 
-@pytest.mark.parametrize('url', [
-    '/clients',
-    '/clients/1'
-])
+
+@pytest.mark.parametrize("url", ["/clients", "/clients/1"])
 def test_get_endpoints(client, url):
     response = client.get(url)
     assert response.status_code == 200
 
-def test_create_client(client,db):
-    response = client.post('/clients', json={'name': 'Martin', 'surname': 'Ankru'})
+
+def test_create_client(client, db):
+    response = client.post("/clients", json={"name": "Martin", "surname": "Ankru"})
     assert response.status_code == 201
     data = response.get_json()
-    assert 'id' in data
+    assert "id" in data
+
 
 def test_create_parking(client):
-    response = client.post('/parkings', json={'address': 'My Address', 'count_places': 5})
+    response = client.post(
+        "/parkings", json={"address": "My Address", "count_places": 5}
+    )
     assert response.status_code == 201
+
 
 @pytest.mark.parking
 def test_enter_parking(client, db):
     # Нужно использовать существующего клиента и парковку с доступными мпестами
-    response = client.post('/client_parkings', json={'client_id': 1, 'parking_id': 1})
+    response = client.post("/client_parkings", json={"client_id": 1, "parking_id": 1})
     assert response.status_code == 201
+
 
 @pytest.mark.parking
 def test_exit_parking(client, db):
-    response = client.delete('/client_parkings', json={'client_id': 1, 'parking_id': 1})
+    response = client.delete("/client_parkings", json={"client_id": 1, "parking_id": 1})
     assert response.status_code == 200
-
