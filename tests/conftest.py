3de064c2ab3b47b5c24dd9e73229c100af7fa1cc
Martin <martinankru927@gmail.com>
2025-11-06 01:12:49 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,21 +3,36 @@ from app import create_app, db as _db
 from app.models import Client, Parking, ClientParking
 from datetime import datetime, timedelta
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def app():
     app = create_app()
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.app_context():
         _db.create_all()
         # Сщздаем тестовые данные
-        client = Client(name='Test', surname='User',
-                        credit_card='1234567890123456', car_number='A123BC')
-        parking = Parking(address='Test Address', opened=True, count_places=10, count_available_places=10)
+        client = Client(
+            name="Test",
+            surname="User",
+            credit_card="1234567890123456",
+            car_number="A123BC",
+        )
+        parking = Parking(
+            address="Test Address",
+            opened=True,
+            count_places=10,
+            count_available_places=10,
+        )
         _db.session.add_all([client, parking])
         _db.session.commit()
 
         # Лог заезда и выезда
-        cp = ClientParking(client_id=client.id, parking_id=parking.id, time_in=datetime.now() - timedelta(hours=1), time_out=datetime.now())
+        cp = ClientParking(
+            client_id=client.id,
+            parking_id=parking.id,
+            time_in=datetime.now() - timedelta(hours=1),
+            time_out=datetime.now(),
+        )
         _db.session.add(cp)
         _db.session.commit()
 
@@ -26,9 +41,11 @@ def app():
         with app.app_context():
             _db.drop_all()
 
+
 @pytest.fixture
 def client(app):
     return app.test_client()
+
 
 @pytest.fixture
 def db(app):
