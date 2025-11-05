5eccd8b8a8ef0e79d3bdfa881ca5510d486098df
Martin <martinankru927@gmail.com>
2025-11-06 01:11:30 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,12 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
-    from .models import Client, Parking, ClientParking
     from .routes import bp
+
     app.register_blueprint(bp)
 
     with app.app_context():
