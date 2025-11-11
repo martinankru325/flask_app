dcb02d69980313251d48c0de1ce1860ecbd0a715
Martin <martinankru927@gmail.com>
2025-11-11 23:37:28 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class Parking(db.Model):
 class ClientParking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.Integer, db.ForeignKey("client.id"))
-    parking_id = db.Column(db.Integer, db.ForegnKey("parking.id"))
+    parking_id = db.Column(db.Integer, db.ForeignKey("parking.id"))
     time_in = db.Column(db.DateTime)
     time_out = db.Column(db.DateTime)
     __table_args__ = (
